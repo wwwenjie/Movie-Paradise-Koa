dca405b461690ce0c0c8ed3be12021b7082f9803
wenjie <jinwenjie@live.com>
2020-05-11 16:20:19 +0800

    feat(core/crawler): add crawl by path

--- src/core/crawler.ts
+++ src/core/crawler.ts
@@ -4,93 +4,150 @@ import logger from './log4js'
 import InitManager from './init'
 import Movie from '../models/movie'
 
-// todo: add crawl by path(ids dont have backdrops attribute)
 // todo: upload poster to alicloud oss and fast.io
 // todo: back up database function
 class GetMovieFromAPI {
+  // fastMode dont have backdrops,use ids: number. set to false to use path: string
+  private static readonly fastMode: boolean = true
   // store id already added, avoid unnecessary query
-  private static readonly done: Set<number>=new Set()
-  private static readonly task: Set<number>=new Set()
-  private static readonly msg: object={ email: 'jinwenjie@live.com', msg: 'Hello, I am getting your data through program, because there is no robots, please contact me if it bothers you' }
+  private static readonly done: Set<number|string> = new Set()
+  private static readonly task: Set<number|string> = new Set()
+  private static readonly msg: object = { email: 'jinwenjie@live.com', msg: 'Hello, I am getting your data through program, because there is no robots, please contact me if it bothers you' }
+  private static url: string
 
   public static async go (): Promise<void> {
+    await InitManager.initLoadDatabase()
+    await this.initTask()
+    const timeout = this.fastMode ? 15000 : 5000
     setInterval(() => {
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
       this.handleTask()
-    }, 15000)
-    await InitManager.initLoadDatabase()
-    await this.initTask()
-    await this.handleTask()
+    }, timeout)
   }
 
   private static async handleTask (): Promise<void> {
-    const ids = []
-    let counter = 0
-    this.task.forEach((id) => {
-      if (counter < 15) {
-        ids.push(id)
-        this.task.delete(id)
-        counter = counter + 1
-      }
-    })
-    await this.getMovies(this.parseIds(ids))
+    if (this.fastMode) {
+      const ids = []
+      let counter = 0
+      this.task.forEach((id) => {
+        if (counter < 15) {
+          ids.push(id)
+          this.task.delete(id)
+          counter = counter + 1
+        }
+      })
+      await this.getMovies(this.parseIds(ids))
+    } else {
+      const path = [...this.task][0]
+      this.task.delete(path)
+      await this.getMovies(path.toString())
+    }
   }
 
-  private static async getMovies (movieIds: string): Promise<void> {
+  private static async getMovies (query: string): Promise<void> {
     try {
-      // relieve server pressure
-      await this.sleep(5000)
-      const res = await superagent.get(`https://api.dianying.fm/movies?ids=${movieIds}`).query(this.msg)
-      logger.info(`get movies from https://api.dianying.fm/movies?ids=${movieIds}`)
-      await Promise.all(res.body.map(async (movie) => {
-        try {
-          movie.trailers = await this.getTrailer(movie._id.toString())
-        } catch (e) {
-          logger.error(e)
-        }
-        try {
-          const movieRes = await MovieService.create(movie)
-          logger.info(`created: ${movieRes.title} ${movieRes._id}`)
-        } catch (err) {
-          logger.error(err)
-        } finally {
-          // add this movie to done
-          this.done.add(movie._id)
-          // remove this movie form task
-          this.task.delete(movie._id)
-          // add recs movies to task
-          movie.recs.map(id => {
-            if (!this.done.has(id)) {
-              this.task.add(id)
-            }
-          })
-        }
-      }))
+      const res = await superagent.get(this.url + query).query(this.msg)
+      if (this.fastMode) {
+        await this.addMovieById(res.body)
+      } else {
+        await this.addMovieByPath(res.body)
+      }
+      logger.info(`get movies from ${this.url}${query}`)
     } catch (err) {
       logger.error(err)
     }
   }
 
+  private static async addMovieById (movies): Promise<void> {
+    await Promise.all(movies.map(async (movie) => {
+      // get movie trailer
+      if (!this.fastMode) {
+        try {
+          movie.trailers = await this.getTrailer(movie._id.toString())
+        } catch (e) {
+          logger.error(e)
+        }
+      }
+      // create movie
+      try {
+        const movieRes = await MovieService.create(movie)
+        logger.info(`created: ${movieRes.title} ${movieRes._id}`)
+      } catch (err) {
+        logger.error(err)
+      } finally {
+        // add this movie to done
+        this.done.add(movie._id)
+        // remove this movie form task
+        this.task.delete(movie._id)
+        // add recs movies to task
+        movie.recs.map(id => {
+          if (!this.done.has(id)) {
+            this.task.add(id)
+          }
+        })
+      }
+    }))
+  }
+
+  private static async addMovieByPath (movie): Promise<void> {
+    // get movie trailer
+    try {
+      movie.trailers = await this.getTrailer(movie._id.toString())
+    } catch (e) {
+      logger.error(e)
+    }
+    // create movie
+    try {
+      const movieRes = await MovieService.create(movie)
+      logger.info(`created: ${movieRes.title} ${movieRes._id}`)
+    } catch (err) {
+      logger.error(err)
+    } finally {
+      // add this movie to done
+      this.done.add(movie.path)
+      // remove this movie form task
+      this.task.delete(movie.path)
+      // add recs movies path to task
+      if (this.task.size < 15) {
+        const res = await superagent.get(`https://api.dianying.fm/movies?ids=${this.parseIds(movie.recs)}`).query(this.msg)
+        res.body.map(movie => {
+          if (!this.done.has(movie.path)) {
+            this.task.add(movie.path)
+          }
+        })
+      }
+    }
+  }
+
   private static async initTask (): Promise<void> {
-    const id = await Movie.findAll({
+    // set url by mode
+    this.url = this.fastMode ? 'https://api.dianying.fm/movies?ids=' : 'https://api.dianying.fm/movies/'
+    // get least movie as init movie
+    // make sure you have at least one movie in database
+    const initMovie = await Movie.findAll({
       limit: 1,
       order: [
         ['create_time', 'DESC']
       ]
     })
     logger.info('========start========')
-    logger.info(Date.now())
-    if (id.length === 0) {
-      // return almost a comedy as init id
-      logger.info('init id is 30269016')
-      this.task.add(30269016)
-    } else {
-      // return lastest id
-      logger.info(`init id is ${id[0].recs.toString()}`)
-      id[0].recs.map(id => {
+    logger.info(new Date(Date.now()))
+    logger.info(`crawler mode: ${this.fastMode ? 'fast' : 'fully'}`)
+    // return init param
+    if (this.fastMode) {
+      logger.info(`init ids is ${initMovie[0].recs.toString()}`)
+      initMovie[0].recs.map(id => {
         this.task.add(id)
       })
+    } else {
+      logger.info(`init path is ${initMovie[0].path}`)
+      this.task.add(initMovie[0].path)
     }
+    // load done
+    const doneMovies = await Movie.findAll({ attributes: ['_id', 'path'] })
+    doneMovies.map(movie => {
+      this.done.add(this.fastMode ? movie._id : movie.path)
+    })
   }
 
   private static parseIds (ids: number[]): string {
